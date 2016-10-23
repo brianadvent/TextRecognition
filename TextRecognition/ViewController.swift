f7e9ed02d804564b97ccd3a273dbfa6ef19b15c4
Training <training@daniels-imac-2.fritz.box>
2016-10-23 14:48:54 +0200

    Textrecognition with Tesseract OCR Tutorial

--- TextRecognition/ViewController.swift
+++ TextRecognition/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import TesseractOCR
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, G8TesseractDelegate {
 
+    @IBOutlet weak var textView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        if let tesseract = G8Tesseract(language: "eng") {
+            tesseract.delegate = self
+            tesseract.image = UIImage(named: "demoText")?.g8_blackAndWhite()
+            tesseract.recognize()
+            
+            textView.text = tesseract.recognizedText
+            
+        }
+        
+        
+    }
+    
+    func progressImageRecognition(for tesseract: G8Tesseract!) {
+        print("Recognition Progress \(tesseract.progress) %")
     }
 
     override func didReceiveMemoryWarning() {
